0f0436a873a53a3a78cb0cfd5a8164773ab4d7ba
Jessica Wolff <jleahwolff@gmail.com>
2019-12-06 11:25:15 -0600

    Stuck on the Live Server not pulling in my data. The console is correctly pulling.

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,4 +1,4 @@
-// STEP 1: Create a header component.
+//STEP 1: Create a header component.
 // -----------------------
 // Using a function create the component you see below:
 //
@@ -8,25 +8,43 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
-function Header(headData) {
+const headerData = [
+    {
+    theDate: 'SMARCH 28, 2019',
+    head: 'Lambda Times',
+    temperature: '98°'
+    }];
+
+
+const headerEntry = document.querySelector('.header-container');
+
+function Header(headerData) {
     //variables--------
     const headerDiv = document.createElement('div');
     //inside the div---
-    const spanDate = document.createElement('date');
+    const spanDate = document.createElement('span');
     const headerOne = document.createElement('h1');
-    const spanTemp = document.createElement('temp');
+    const spanTemp = document.createElement('span');
     //-----------------
     //classes----------
-    div.classList.add('header');
-    span.classList.add('date');
-    span.classList.add('temp');
+    headerDiv.classList.add('.header');
+    spanDate.classList.add('.date');
+    spanTemp.classList.add('.temp');
     //----------------
     //append----------
-    headerDiv.append(spanDate);
-    headerDiv.append(headerOne);
-    headerDiv.append(spanTemp);
+    headerDiv.appendChild(spanDate);
+    headerDiv.appendChild(headerOne);
+    headerDiv.appendChild(spanTemp);
     //----------------
     //text content----
-    
+    spanDate.textContent = headerData.theDate;
+    headerOne.textContent = headerData.head;
+    spanTemp.textContent = headerData.temperature;
     //----------------
-    }
+    //entrypoint------
+    headerEntry.appendChild(headerDiv);
+    //----------------
+    //return----------
+    return headerDiv;
+    //----------------
+    };
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -4,6 +4,44 @@
 // Once the data is returned console.log it and review the structure.
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
-//
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+const tabsEntry = document.querySelector('.topics');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(response => {
+    theTabs(response.data)
+    console.log(response.data);
+    console.log(response)
+})
+.catch(err => {
+    console.log(err)
+});
+
+function theTabs (data){
+//variables
+const divTabs = document.createElement('div');
+const divTopics = document.createElement('div');
+const divTab = document.createElement('div');
+const spanTitle = document.createElement('span');
+
+//class list
+divTabs.classList.add('tabs');
+divTopics.classList.add('topics');
+divTab.classList.add('tab');
+spanTitle.classList.add('title');
+
+//appending
+divTabs.append(divTopics);
+divTopics.append(spanTitle);
+divTopics.append(divTab);
+
+//text content
+spanTitle.textContent = `Trending Topics:`;
+divTab.textContent = data.topics;
+
+tabsEntry.append(divTopics);
+console.log(divTopics);
+return divTopics;
+};
+
